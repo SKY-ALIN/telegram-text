9976a4481aec78604c17f0f310501a57b417b386
Vladimir Alinsky <adam3iks@gmail.com>
2022-05-07 21:32:57 +0800

    Add styling test for Hashtag

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -1,4 +1,6 @@
 from telegram_text.elements import Hashtag
+from telegram_text.bases import PlainText
+from telegram_text.styles import Bold, Italic
 
 
 def test_creation():
@@ -8,3 +10,11 @@ def test_creation():
     assert Hashtag("#hashtag").to_markdown() == "#hashtag"
     assert Hashtag("hashtag").to_html() == "#hashtag"
     assert Hashtag("#hashtag").to_html() == "#hashtag"
+
+
+def test_style_fabric():
+    assert Hashtag("hashtag", style_fabric=PlainText).to_markdown() == "#hashtag"
+    assert Hashtag("hashtag", style_fabric=Bold).to_markdown() == "*#hashtag*"
+    assert Hashtag("hashtag", style_fabric=Italic).to_markdown() == "_#hashtag_"
+    assert Hashtag("hashtag", style_fabric=lambda string: Bold(Italic(string))).to_markdown() == "*_#hashtag_*"
+    assert Hashtag("hashtag", style_fabric=lambda string: Bold(Italic(string))).to_plain_text() == "#hashtag"
