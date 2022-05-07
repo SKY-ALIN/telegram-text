fb36ca31ebad5c4d38502d4040262786defe31a1
Vladimir Alinsky <adam3iks@gmail.com>
2022-05-07 19:13:53 +0800

    Add structure test for Chain

---
+++ tests/test_chain.py
@@ -0,0 +1,29 @@
+from telegram_text.elements import Chain
+from telegram_text.styles import Bold, Italic, InlineCode
+
+
+def test_chain_structure():
+    bold = Bold("bold text")
+    italic = Italic("italic text")
+    code = InlineCode("Some code...")
+    chain = Chain(bold, italic, code)
+    assert isinstance(chain, Chain)
+    assert bold in chain
+    assert italic in chain
+    assert code in chain
+
+    assert chain.to_plain_text() == (
+        bold.to_plain_text() + ' ' +
+        italic.to_plain_text() + ' ' +
+        code.to_plain_text()
+    )
+    assert chain.to_markdown() == (
+        bold.to_markdown() + ' ' +
+        italic.to_markdown() + ' ' +
+        code.to_markdown()
+    )
+    assert chain.to_html() == (
+        bold.to_html() + ' ' +
+        italic.to_html() + ' ' +
+        code.to_html()
+    )
