52e964899a0d60dee85908a875c287a01be403df
SKY-ALIN <vladimir@alinsky.tech>
2024-01-13 18:13:53 +0300

    Add `Quote` style

--- telegram_text/__init__.py
+++ telegram_text/__init__.py
@@ -2,7 +2,7 @@ from .bases import Chain, PlainText, Text
 from .custom import TOMLSection
 from .elements import Emoji, Hashtag, InlineUser, Link, User
 from .markdown import OrderedList, UnorderedList
-from .styles import Bold, Code, InlineCode, Italic, Spoiler, Strikethrough, Underline
+from .styles import Bold, Code, InlineCode, Italic, Quote, Spoiler, Strikethrough, Underline
 
 __version__ = '0.1.2'
 __all__ = [
@@ -14,6 +14,7 @@ __all__ = [
     "Code",
     "InlineCode",
     "Italic",
+    "Quote",
     "Spoiler",
     "Strikethrough",
     "Underline",
